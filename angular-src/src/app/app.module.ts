6811c0a1fe20614127bde12a68b3931dbc24e2d2
nedemenang <nnamso.edemenang@gmail.com>
2018-09-25 17:52:19 +0100

    first commit

---
+++ angular-src/src/app/app.module.ts
@@ -0,0 +1,52 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router'
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RegisterComponent } from './register/register.component';
+
+import {ValidateService} from './services/validate.service';
+import {AuthService} from './services/auth.service'
+import {FlashMessagesModule} from 'angular2-flash-messages';
+import {AuthGuard} from './guards/auth.guard';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'dashboard', component: DashboardComponent, canActivate:[AuthGuard]},
+  {path: 'profile', component: ProfileComponent, canActivate:[AuthGuard]},
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    HomeComponent,
+    LoginComponent,
+    ProfileComponent,
+    RegisterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+    FlashMessagesModule.forRoot()
+  ],
+  providers: [
+    ValidateService,
+    AuthService,
+    AuthGuard
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
